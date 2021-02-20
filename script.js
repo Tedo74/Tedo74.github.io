c55da63e1b00588af50290889f1683a624eca2ff
tedo74 <teddy2@abv.bg>
2021-02-20 20:44:05 +0200

    design changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const li = document.querySelectorAll('.exp-dates');
 li.forEach((el) =>
   el.addEventListener('click', () => {
     infoToShow = el.querySelector('.experience-details');
-    if (infoToShow.style.display === 'none') {
+    if (infoToShow.style.display !== 'block') {
       infoToShow.style.display = 'block';
     } else {
       infoToShow.style.display = 'none';
