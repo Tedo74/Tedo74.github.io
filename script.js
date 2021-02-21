677ce6ff97995afacfd5f229704edacc597361c8
tedo74 <teddy2@abv.bg>
2021-02-21 12:27:14 +0200

    script changed

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const li = document.querySelectorAll('.exp-dates');
 li.forEach((el) =>
   el.addEventListener('click', () => {
     infoToShow = el.querySelector('.experience-details');
-    if (infoToShow.style.display !== 'block') {
-      infoToShow.style.display = 'block';
-    } else {
-      infoToShow.style.display = 'none';
-    }
+    infoToShow.classList.toggle('show-detail');
   })
 );
